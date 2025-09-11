d008e76fe1a4f6f5dca5a65846be6406371b5c39
FelipeBicaletto <febicaletto@gmail.com>
2025-09-11 11:13:42 -0300

    Textos do Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,14 +5,31 @@ const Home = () => {
   return (
     <main>
         <img src={Imagem1} alt="foto1" className='w-screen'/>
-        <h2 className='flex justify-center font-bold text-purple-600 text-3xl'>Quem somos?</h2>
-        <div className='w-200 flex justify-center p-4'>
-          <p className='overflow-hidden text-clip'> O Passa a Bola é um projeto de mídia independente que dá visibilidade ao esporte feminino, especialmente ao futebol.
+
+        <h2 className='flex justify-center font-bold text-purple-600 text-3xl mt-10'>Quem somos?</h2>
+
+        {/* Texto sobre o Passa a Bola */}
+        <div className='flex justify-center items-center text-center p-1 mt-13'>
+          <p className='max-w-2xl'> O Passa a Bola é um projeto de mídia independente que dá visibilidade ao esporte feminino, especialmente ao futebol.
             Criado por Alê Xavier e Luana Maluf, o canal promove transmissões, entrevistas, reportagens e conteúdos digitais com foco em valorizar atletas mulheres, 
             combater o machismo no esporte e ampliar a presença feminina na mídia esportiva.</p>
+        </div>
+
+        {/* Texto sobro os Torneios */}
+        <div className='relative flex justify-center mt-17 group'> {/* Div pai para centralizar a section */}
+
+          {/* Dic sombra */}
+          <div className='absolute top-3 left-132 bg-purple-800 rounded-[50px] max-w-3xl w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-700'></div>
+
+          <div className='relative flex bg-gradient-to-r from-purple-500 to-purple-600 max-w-3xl w-full justify-center items-center flex-col text-center p-6 rounded-[50px] transition-transform duration-700 group-hover:translate-x-1 group-hover:translate-y-1'>
+            <h2 className='flex justify-center font-bold text-white text-3xl'>Sobre os torneios</h2>
+
+            <p className='text-white mt-10'> O Passa a Bola organiza torneios de futebol feminino que valorizam a presença das mulheres no esporte.
+              Os eventos reúnem jogadoras amadoras, influenciadoras e entusiastas em partidas descontraídas, 
+              promovendo empoderamento e criando um ambiente acolhedor e seguro para diferentes perfis e estilos de jogo.</p>
+          </div>
         </div>
     </main>
   )
 }
-
 export default Home

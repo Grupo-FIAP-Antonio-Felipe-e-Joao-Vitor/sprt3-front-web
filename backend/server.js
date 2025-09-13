79009c2c7694c03ea87b95df77b6ef1ec26a8f51
Antonio <antoniojacintoa@gmail.com>
2025-09-13 15:53:32 -0300

    rota criando usuario

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const { v4: uuid } = require("uuid")
 
 const app = express();
 
@@ -10,6 +11,7 @@ const PORT = 3001;
 
 const usuarios = [
     {
+        id: 1,
         nome: "Teste Nome",
         email: "Teste Email",
         username: "Teste Username",
@@ -25,6 +27,21 @@ app.get("/usuarios", (req, res) => {
         res.status(500).json({ error: `Erro interno. ${error}` })
     }
 });
+
+app.post("/usuarios", (req, res) => {
+    const { nome, email, username, senha, role } = req.body;
+    try {
+        if (nome && email && username && senha && role) {
+            novoUsuario = { id: uuid(), nome: nome, email: email, username: username, senha: senha, role: role };
+            usuarios.push(novoUsuario)
+            res.status(201).json({ message: "Usuário criado." });
+        } else {
+            res.status(400).json({ error: "Todos os dados são obrigatórios" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: `Erro interno. ${error}` })
+    }
+})
 
 
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));

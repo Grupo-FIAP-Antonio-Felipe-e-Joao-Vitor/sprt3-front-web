6d62c74115a98cbf390bb2acd25978436605ae87
Antonio <antoniojacintoa@gmail.com>
2025-09-13 15:46:08 -0300

    rota usuarios backend

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,23 @@ app.use(cors());
 
 const PORT = 3001;
 
+const usuarios = [
+    {
+        nome: "Teste Nome",
+        email: "Teste Email",
+        username: "Teste Username",
+        senha: "SenhaTeste",
+        role: "User"
+    },
+];
+
+app.get("/usuarios", (req, res) => {
+    try {
+        res.status(200).json(usuarios);
+    } catch (error) {
+        res.status(500).json({ error: `Erro interno. ${error}` })
+    }
+});
+
+
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
